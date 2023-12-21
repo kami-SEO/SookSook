10ddc1e4a5d1a755cabaf0d9d6e923f1b5615f82
SKM <rkdalsv@naver.com>
2023-12-21 19:48:30 +0900

    버튼추가

--- app/src/main/java/com/example/sooksook/MainActivity.kt
+++ app/src/main/java/com/example/sooksook/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+
 
         binding.clkBtn
     }
