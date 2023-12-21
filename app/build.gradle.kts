465bea7573ec6b7ba19592038019e5b365683312
SKM <rkdalsv@naver.com>
2023-12-21 21:01:46 +0900

    로그 찍어보기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sooksook"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sooksook"
         minSdk = 16
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/sooksook/MainActivity.kt
+++ app/src/main/java/com/example/sooksook/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sooksook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sooksook.databinding.ActivityMainBinding
 
 
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        binding.clkBtn
+        binding.clkBtn.setOnClickListener {
+
+            //주석 ctrl + /
+//            {}안의 코드만 눌렀을 때 실행
+            Log.d("메인화면로그", "클릭용버튼으로 눌림")
+        }
+        binding.smallBtn
     }
 }
